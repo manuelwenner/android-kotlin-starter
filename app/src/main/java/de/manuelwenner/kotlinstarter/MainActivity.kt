99afcc29425881f9a93392e89c497fb2291295d0
Manuel Wenner <hi@manuelwenner.de>
2024-04-11 10:55:32 +0200

    feat: add imageview and show on button click

--- app/src/main/java/de/manuelwenner/kotlinstarter/MainActivity.kt
+++ app/src/main/java/de/manuelwenner/kotlinstarter/MainActivity.kt
@@ -2,11 +2,9 @@ package de.manuelwenner.kotlinstarter
 
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,6 +13,7 @@ class MainActivity : AppCompatActivity() {
 
         findViewById<Button>(R.id.btnSubmit).setOnClickListener{
             findViewById<TextView>(R.id.tvContent).text = "Yaaay it worked"
+            findViewById<ImageView>(R.id.ivBatman).animate().scaleX(1.0F).duration = 2000
         }
     }
 }
